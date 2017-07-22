80446376676e96c792100b98fb958aad558decd1
pingzing <mcalistern@gmail.com>
2017-07-23 01:53:32 +0300

    Swap out ANSI methods for Unicode ones

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::ffi::CString;
 use std::env;
 use std::process::Command;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::iter::once;
 
 extern crate winapi;
 extern crate user32;
-
 
 fn main() {
     match env::args().nth(1) {
@@ -25,8 +26,8 @@ fn main() {
 
 unsafe extern "system" fn enumerate_callback(
     hwnd: winapi::HWND, 
-    lparam: winapi::LPARAM) 
-    -> winapi::BOOL {    
+    _: winapi::LPARAM) 
+    -> winapi::BOOL {        
         let title_length = user32::GetWindowTextLengthW(hwnd) + 1;
         let mut v: Vec<u16> = Vec::with_capacity(title_length  as usize);
         let read_len = user32::GetWindowTextW(hwnd, v.as_mut_ptr(), title_length);
@@ -34,10 +35,10 @@ unsafe extern "system" fn enumerate_callback(
         // Not all windows have titles, ignore those that don't
         if read_len > 0 {
             v.set_len((read_len) as usize);
-            let string = String::from_utf16_lossy(&v);            
-            if string.contains("VLC media player") {
-                let window_name = CString::new(string).unwrap();    
-                let window_handle = user32::FindWindowA(std::ptr::null_mut(), window_name.as_ptr());
+            let string = String::from_utf16_lossy(&v);
+            let wide_string: Vec<u16> = OsStr::new(string.as_str()).encode_wide().chain(once(0)).collect();
+            if string.contains("VLC media player") {                                
+                let window_handle = user32::FindWindowW(std::ptr::null_mut(), wide_string.as_ptr());
                 user32::SetForegroundWindow(window_handle);
                 user32::ShowWindow(window_handle, 9);
                 return winapi::FALSE;
