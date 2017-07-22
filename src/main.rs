4c3b71365d34924a0f5964420e14fdda81f08d21
pingzing <mcalistern@gmail.com>
2017-07-22 14:15:17 +0300

    Get callback working correctly

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,32 @@ extern crate winapi;
 extern crate user32;
 
 fn main() {
-    let window_name = CString::new("VLC media player").unwrap();
-    let window_handle;
-    unsafe {
-        window_handle = user32::FindWindowA(std::ptr::null_mut(), window_name.as_ptr());        
-        user32::SetForegroundWindow(window_handle);
-        user32::ShowWindow(window_handle, 9);
+    
+    unsafe {      
+        user32::EnumWindows(Some(enumerate_callback), 0i64);        
     }
+}
+
+unsafe extern "system" fn enumerate_callback(
+    hwnd: winapi::HWND, 
+    lparam: winapi::LPARAM) 
+    -> winapi::BOOL {    
+        let title_length = user32::GetWindowTextLengthW(hwnd) + 1;
+        let mut v: Vec<u16> = Vec::with_capacity(title_length  as usize);
+        let read_len = user32::GetWindowTextW(hwnd, v.as_mut_ptr(), title_length);
+
+        // Not all windows have titles, ignore those that don't
+        if read_len > 0 {
+            v.set_len((read_len) as usize);
+            let string = String::from_utf16_lossy(&v);            
+            if string.contains("VLC media player") {
+                let window_name = CString::new(string).unwrap();    
+                let window_handle = user32::FindWindowA(std::ptr::null_mut(), window_name.as_ptr());
+                user32::SetForegroundWindow(window_handle);
+                user32::ShowWindow(window_handle, 9);
+                return winapi::FALSE;
+            }
+        }        
+        
+        return winapi::TRUE;                                
 }
